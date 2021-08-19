2b87261ebe1bfe44289f9825467de8e0de6b51a6
olegsolomin <solomin.o.n@gmail.com>
2021-08-19 15:13:31 +0300

    myInfo ammendments

--- src/public/sign-up/myInfo.controller.js
+++ src/public/sign-up/myInfo.controller.js
@@ -10,10 +10,15 @@ function MyInfoController(SignUpService) {
   var myInfo = this;
   myInfo.persons = SignUpService.showInfo();
 
-  // var promise = SignUpService.getfavitem(myInfo.persons[0].favitem);
-  // promise.then (function (response) {
-  //     myInfo.item = response.data;
-  // });
+try {
+  var promise = SignUpService.getfavitem(myInfo.persons[0].favitem);
+  promise.then (function (response) {
+      myInfo.item = response.data;
+  });
+} catch (e) {
+  console.log("no such item yet");
+};
+
 
 
   myInfo.IsEmpty = function () {
